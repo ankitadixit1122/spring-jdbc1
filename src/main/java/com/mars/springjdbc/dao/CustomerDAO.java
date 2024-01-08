c982f8257e8175daf276c2e1817d8b9281ed66d3
ANKU <ankitadixit1122@gmail.com>
2024-01-08 13:18:05 -0600

    customer id serach added

--- src/main/java/com/mars/springjdbc/dao/CustomerDAO.java
+++ src/main/java/com/mars/springjdbc/dao/CustomerDAO.java
@@ -6,6 +6,8 @@ import com.mars.springjdbc.model.Customer;
 
 public interface CustomerDAO {
 	
+	//THIS WILL CALL IMPLEMENTATION CLASS USING RUNTIME POLYMERPHISMS
+	
 	Customer getCustomerById(int customer_id );
 	
 	List<Customer> getAllCustomers();
--- src/main/java/com/mars/springjdbc/dao/CustomerDAOImpl.java
+++ src/main/java/com/mars/springjdbc/dao/CustomerDAOImpl.java
@@ -1,11 +1,11 @@
 package com.mars.springjdbc.dao;
-
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import com.mars.springjdbc.model.Customer;
@@ -22,6 +22,7 @@ public class CustomerDAOImpl implements CustomerDAO {
 	private final String SQL_UPDATE_CUSTOMER = "update customer set customer_name =? ,city =?,grade =?,salesman_id=? where customer_id =?";
 	private final String SQL_DELETE_CUSTOMER = "delete from customer where customer_id =?";
 	private final String SQL_SELECT_ALL_CUSTOMER ="select * from customer";
+	private final String SQL_SELECT_SINGLE_CUSTOMER ="select * from customer where customer_id = ?";
 	
 	@Autowired
 	public CustomerDAOImpl(DataSource ds) {
@@ -29,9 +30,14 @@ public class CustomerDAOImpl implements CustomerDAO {
 		
 	}
 	
+	
+	//SELECT SINGLE CUSTOMER DATA
 	public Customer getCustomerById(int customer_id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		//ROWMAPER IS AN INTERFACE SO WE NEED TO CREATE A IMPLEMRNTATION CLASS TO CREATE OBJECT OF ROWMAPPER THAT IS CUSTOMERMAPPER
+		RowMapper<Customer> rowMapper =new CustomerMapper();
+		Customer customer =this.jdbcTemplate.queryForObject(SQL_SELECT_SINGLE_CUSTOMER, rowMapper, customer_id);
+		return customer;
 	}
 
 	
